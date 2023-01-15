279e078e62f14df56befd848d5d312768181b0f3
ActualAngel1 <115042939+ActualAngel1@users.noreply.github.com>
2023-01-15 22:24:52 +0200

    Finished The Resolver

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -1,12 +1,17 @@
 package com.craftinginterpreters.lox;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Interpreter implements Expr.Visitor<Object>,
         Stmt.Visitor<Void> {
     final Environment globals = new Environment();
     private Environment environment = globals;
+    private final Map<Expr, Integer> locals = new HashMap<>();
 
     Interpreter() {
         globals.define("clock", new LoxCallable() {
@@ -73,7 +78,15 @@ class Interpreter implements Expr.Visitor<Object>,
     }
     @Override
     public Object visitVariableExpr(Expr.Variable expr) {
-        return environment.get(expr.name);
+        return lookUpVariable(expr.name, expr);
+    }
+    private Object lookUpVariable(Token name, Expr expr) {
+        Integer distance = locals.get(expr);
+        if (distance != null) {
+            return environment.getAt(distance, name.lexeme);
+        } else {
+            return globals.get(name);
+        }
     }
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
@@ -198,6 +211,9 @@ class Interpreter implements Expr.Visitor<Object>,
     private void execute(Stmt stmt) {
         stmt.accept(this);
     }
+    void resolve(Expr expr, int depth) {
+        locals.put(expr, depth);
+    }
     @Override
     public Void visitBlockStmt(Stmt.Block stmt) {
         executeBlock(stmt.statements, new Environment(environment));
@@ -274,7 +290,14 @@ class Interpreter implements Expr.Visitor<Object>,
     @Override
     public Object visitAssignExpr(Expr.Assign expr) {
         Object value = evaluate(expr.value);
-        environment.assign(expr.name, value);
+
+        Integer distance = locals.get(expr);
+        if (distance != null) {
+            environment.assignAt(distance, expr.name, value);
+        } else {
+            globals.assign(expr.name, value);
+        }
+
         return value;
     }
     @Override
--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -55,6 +55,12 @@ public class Lox {
             // Stop if there was a syntax error.
             if (hadError) return;
 
+            Resolver resolver = new Resolver(interpreter);
+            resolver.resolve(statements);
+
+            // Stop if there was a resolution error.
+            if (hadError) return;
+
             // I want the REPL session to print expressions
             if (isREPL) runREPL(statements);
             else interpreter.interpret(statements);
